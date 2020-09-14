edb2250013fb6e4fced57a26587d5d9805037f3c
qlanto <qlanto_147@163.com>
2020-09-14 11:37:33 +0800

    Revert "初始"
    This reverts commit 4193dfac

--- src/App.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div id="app">
-        <router-view />
-        <!-- <keep-alive >
-        <router-view v-if="$route.meta.keepAlive"/>
-        </keep-alive><router-view v-if="!$route.meta.keepAlive"/> -->
-    </div>
-</template>
